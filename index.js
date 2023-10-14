a9276dca6d11fa803c1d60e8c2176a052787a03b
gustavobedimm <gustavo_bmazutti@homtail.com>
2023-10-14 17:56:03 -0300

    install cors

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const nodemailer = require("nodemailer");
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
