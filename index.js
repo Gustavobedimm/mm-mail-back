62015fa3393b9c72cc75ec542c3e235928dc468a
gustavobedimm <gustavo_bmazutti@hotmail.com>
2023-10-14 16:11:15 -0300

    fist commit

---
+++ index.js
@@ -0,0 +1,36 @@
+
+const express = require('express');
+const app = express();
+const nodemailer = require("nodemailer");
+const PORT = process.env.PORT || 5000;
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'mudancasmazutti@gmail.com',
+      pass: 'pjri hows xcwl iqzr'
+    }
+  });
+
+  async function main() {
+    const info = await transporter.sendMail({
+      from: '"Mudanças Mazutti" <central.defretes@hotmail.com>', // sender address
+      to: "gustavo_bmazutti@hotmail.com", // list of receivers
+      subject: "Orçamento", // Subject line
+      text: "Por favor não responder este E-mail." // plain text body
+      //html: "<b>Hello world?</b>", // html body
+    });
+}
+app.post("/send-mail", async (req,res) => {
+    main().catch(console.error);
+    return res.json({
+        erro:false,
+        mensagem: "Email enviado com sucesso"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log("servidor iniciado na porta" + PORT);
+})
