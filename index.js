44c3b870e7786595a759e080d49b54a94d3b39b2
gustavobedimm <gustavo_bmazutti@hotmail.com>
2023-10-18 22:17:08 -0300

    FINAL

--- index.js
+++ index.js
@@ -69,9 +69,6 @@ app.post("/send-mail", async (req,res) => {
   //MONTAR O PDF DO ORÇAMENTO
     var docpdf = new PDFDocument();
     //CORPO PDF----------------------------------------------------
-    const logo = await fetchImage("https://lh5.googleusercontent.com/p/AF1QipN6RPbz6va-JuSZYcew0Amv76AKbi8OV0GXgPKN=w203-h203-k-no");
-    docpdf.image(logo, 0, 15, {width: 300})
-   .text('Proportional to width', 0, 0);
     docpdf.fontSize(11);
     docpdf.text("Mudaças Mazutti ME - 01.367.190/0001-42" , { align: 'right'});
     docpdf.text("Rua Parecis 1699, Cascavel-PR" , { align: 'right'});
