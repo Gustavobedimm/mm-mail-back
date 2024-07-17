e90cc12ea5d7212056c8be86083b51b26f14302c
gustavobedimm <gustavo_bmazutti@hotmail.com>
2024-07-17 10:28:59 -0300

    teste imagem

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const PDFDocument = require('pdfkit');
 const app = express();
+import fetch from "node-fetch"
 
 app.use(cors('*'));
 app.use(express.json());
 
 app.post("/send-mail", async (req,res) => {
+  
+  const image_url = "https://scontent-gru2-1.cdninstagram.com/v/t51.2885-19/296477577_428755949187751_5501916957594122246_n.jpg?stp=dst-jpg_s320x320&_nc_ht=scontent-gru2-1.cdninstagram.com&_nc_cat=107&_nc_ohc=6pgBUqRlz_wQ7kNvgFSP6lU&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AYAoTBd6x02wN-amkJhW9FPdHFJKg7CZ9cQekbT6oBLMdg&oe=669D8186&_nc_sid=8b3546";
+  const res = await fetch(image_url);
+  const resBuffer = await res.buffer();
+
+
+
   //monta data
   const months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
   const date = new Date();
@@ -123,6 +131,7 @@ app.post("/send-mail", async (req,res) => {
     //docpdf.moveDown(3);
     //docpdf.text(empresaCidade+"-"+empresaEstado +" , "+ StringdataAtual);
    //novo MODELO
+   docpdf.image(resBuffer, 0, 0, {fit: [250, 300]});
    docpdf.fontSize(20);
    docpdf.text("ORÇAMENTO", { align: "center" });
    docpdf.fontSize(11);
