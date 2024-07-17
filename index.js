a9c3466b4d409976450fa4d701f30e209bba9eb3
gustavobedimm <gustavo_bmazutti@hotmail.com>
2024-07-17 14:48:36 -0300

    teste

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 const PDFDocument = require('pdfkit');
 const app = express();
 
