d887859a4fb12a250e762fa9c53bbed82c1602d9
gustavobedimm <gustavo_bmazutti@hotmail.com>
2024-07-17 15:07:53 -0300

    teste imagem

--- index.js
+++ index.js
@@ -106,8 +106,8 @@ app.post("/send-mail", async (req,res) => {
 
    var docpdf = new PDFDocument();
    docpdf.fontSize(20);
-   docpdf.font("Helvetica-Bold").text("Base64:"+imageBase64, 75, 5);
-   //docpdf.image(data64, 10, 10, {scale: 0.25});
+   //docpdf.font("Helvetica-Bold").text("Base64:"+imageBase64, 75, 5);
+   docpdf.image(imageBase64, 10, 10, {scale: 0.25});
    docpdf.text("ORÇAMENTO", { align: "center" });
    docpdf.fontSize(11);
    docpdf.rect(70, 119, 480, 15).stroke();
